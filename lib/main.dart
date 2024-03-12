b860ca6862875fd71410e5697412b76cb969a655
Vaibhav Hariani <62775035+Vaibhav-Hariani@users.noreply.github.com>
2024-03-11 20:26:22 -0400

    Bets can be accepted!

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 // import '../utils/account.dart';
 // import '../utils/blackjack.dart';
-import 'slots.dart' as slots;
-
+import "appbar.dart";
 // Hard Coding user data for now
 //TODO: Integrate login
-String username = 'admin';
+String userID = '00';
 void main() => runApp(new MyApp());
 
 final globtheme = new ThemeData(scaffoldBackgroundColor: const Color(0xFF000000));
@@ -34,57 +33,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(
-        title: new TextButton(
-            child: Text('♤♡ COOPER CASINO ♢♧',
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 25.0,
-                    fontWeight: FontWeight.bold)),
-            onPressed: () => Navigator.push(
-                context, MaterialPageRoute(builder: (context) => MyApp()))),
-        backgroundColor: const Color(0xFF000000),
-        elevation: 0,
-        actions: [
-          TextButton(
-              child: Text('BLACKJACK',
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 25.0,
-                      fontWeight: FontWeight.bold)),
-              onPressed: () => Navigator.push(
-                  context, MaterialPageRoute(builder: (context) => MyApp()))),
-          Text("|",
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold)),
-
-          TextButton(
-              child: Text('SLOTS',
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 25.0,
-                      fontWeight: FontWeight.bold)),
-              onPressed: () {
-                Navigator.push(
-                context, MaterialPageRoute(builder: (context) => slots.Slots_Home(title: "Play Slots", userID: username,theme: globtheme,)));}
-                  ),
-          Text("|",
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 25.0,
-                  fontWeight: FontWeight.bold)),
-
-          IconButton(
-              icon: const Icon(Icons.account_circle,
-                  color: Colors.white,
-                  size: 40.0,
-                  semanticLabel: 'User Account'),
-              onPressed: () => Navigator.push(
-                  context, MaterialPageRoute(builder: (context) => MyApp()))),
-        ],
-      ),
+      appBar: appBar_get(context),
       body: new Center(
         child: new Column(
           mainAxisAlignment: MainAxisAlignment.center,
--- lib/slots.dart
+++ lib/slots.dart
@@ -1,6 +1,7 @@
+import 'package:front_end/main.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
-
+import 'appbar.dart';
 
 
 class Slots_Home extends StatelessWidget {
@@ -15,28 +16,84 @@ class Slots_Home extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Slots main page',
       theme: this.theme,
-      home: Slots_Frontend(),
+      home: Slots_Frontend(this.userID),
     );
   }
 
 }
 
 class Slots_Frontend extends StatefulWidget {
+
+  Slots_Frontend(this.userID);
+  final String userID;
+  //SET THIS BY Database!
+
   @override
   State<Slots_Frontend> createState() => _Slots_State();
 }
 
 class _Slots_State extends State<Slots_Frontend> {
+
+  int maxbet = 500;
+  int bet_value = -1;
+  bool ready_to_bet = false;
+  bool bet_placed = false;
+
+  bool ValidBet(String value){
+    if(int.tryParse(value) != null && int.parse(value) < maxbet){
+      bet_value  = int.parse(value);
+      print(value + " Is a valid bet!");
+
+      return true;
+    }
+    print(value);
+    return false;
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(
-          style: Theme.of(context).textTheme.displayMedium,
-        ),
+      appBar: appBar_get(context),
+      body: new Center(
+        child: TextField(
+          keyboardType: TextInputType.numberWithOptions(decimal: true),
+          decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            labelText: "Enter your bet here," + userID,
+            labelStyle: TextStyle(
+                    color: Colors.white,
+                    fontSize: 25.0,
+                    fontWeight: FontWeight.bold)),
+          onSubmitted: (String value) {ready_to_bet = ValidBet(value);},
+        )
       ),
+
+
+    );
+    return Column(
+        children:[
+          TextField(
+          keyboardType: TextInputType.numberWithOptions(decimal: true),
+          decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            labelText: "Enter your bet here" + userID,
+          ),
+          onSubmitted: (value) { ready_to_bet = ValidBet(value);}
+            ),
+          ready_to_bet ?
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton(
+                child: Text("Place Bet?"),
+                onPressed:() => bet_placed = true),
+            ],
+          ) : Container(),
+        ],
     );
   }
+
 }
 
 
